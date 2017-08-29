1c6736b48e4f25df4c637fe2cab5ec3ee5c4a34f
Abhinav Vohra <avohra@servicesource.com>
2017-08-29 16:16:28 -0700

    Update salesforce.js

--- salesforce.js
+++ salesforce.js
@@ -31,25 +31,25 @@ let findProperties = (params) => {
     if (params) {
         let parts = [];
         if (params.id) parts.push(`id='${params.id}'`);
-        if (params.city) parts.push(`city__c='${params.city}'`);
-        if (params.bedrooms) parts.push(`beds__c=${params.bedrooms}`);
-        if (params.priceMin) parts.push(`price__c>=${params.priceMin}`);
-        if (params.priceMax) parts.push(`price__c<=${params.priceMax}`);
+        if (params.city) parts.push(`avohra__city__c='${params.city}'`);
+        if (params.bedrooms) parts.push(`avohra__beds__c=${params.bedrooms}`);
+        if (params.priceMin) parts.push(`avohra__price__c>=${params.priceMin}`);
+        if (params.priceMax) parts.push(`avohra__price__c<=${params.priceMax}`);
         if (parts.length>0) {
             where = "WHERE " + parts.join(' AND ');
         }
     }
     return new Promise((resolve, reject) => {
         let q = `SELECT id,
-                    title__c,
-                    address__c,
-                    city__c,
-                    state__c,
-                    price__c,
-                    beds__c,
-                    baths__c,
-                    picture__c
-                FROM property__c
+                    avohra__title__c,
+                    avohra__address__c,
+                    avohra__city__c,
+                    avohra__state__c,
+                    avohra__price__c,
+                    avohra__beds__c,
+                    avohra__baths__c,
+                    avohra__picture__c
+                FROM avohra__property__c
                 ${where}
                 LIMIT 5`;
         org.query({query: q}, (err, resp) => {
@@ -71,16 +71,16 @@ let findPriceChanges = () => {
                     CreatedDate,
                     Field,
                     Parent.Id,
-                    Parent.title__c,
-                    Parent.address__c,
-                    Parent.city__c,
-                    Parent.state__c,
-                    Parent.price__c,
-                    Parent.beds__c,
-                    Parent.baths__c,
-                    Parent.picture__c
+                    Parent.avohra__title__c,
+                    Parent.avohra__address__c,
+                    Parent.avohra__city__c,
+                    Parent.avohra__state__c,
+                    Parent.avohra__price__c,
+                    Parent.avohra__beds__c,
+                    Parent.avohra__baths__c,
+                    Parent.avohra__picture__c
                 FROM property__history
-                WHERE field = 'Price__c'
+                WHERE field = 'avohra__Price__c'
                 ORDER BY CreatedDate DESC
                 LIMIT 3`;
         org.query({query: q}, (err, resp) => {
@@ -102,7 +102,7 @@ let createCase = (propertyId, customerName, customerId) => {
         c.set('description', "Facebook id: " + customerId);
         c.set('origin', 'Facebook Bot');
         c.set('status', 'New');
-        c.set('Property__c', propertyId);
+        c.set('avohra__Property__c', propertyId);
 
         org.insert({sobject: c}, err => {
             if (err) {
