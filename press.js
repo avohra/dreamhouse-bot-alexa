d32c74cc000436b26624298203b278a9bcab9023
avohra <avohra@servicesource.com>
2017-09-07 16:46:00 -0700

    Addding bleep phrases

---
+++ press.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "bullshit"      : ["ˈbʊlˌʃɪt", "bullsheet"],
+    "fuck"          : ["ˈfʌk", "fork"],
+    "fucked"        : ["ˈfʌkd", "forked"],
+    "fucker"        : ["ˈfʌkər", "forka"],
+    "fucking"       : ["ˈfʌkɪŋ", "forking"],
+    "shit"          : ["ˈʃɪt", "sheet"],
+};
