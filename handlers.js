aa7c88e940154c4a66239fd7f5f4bc1fcdac833f
avohra <avohra@servicesource.com>
2017-09-13 10:15:17 -0700

    remove fucking

--- handlers.js
+++ handlers.js
@@ -413,7 +413,7 @@ let SalesRepProgress = (slots, session, response, dialogState) => {
     });
 }
 let WhatShouldIDo = (slots, session, response, dialogState) => {
-    response.say("Stop screwing around and just fucking call the end user!");
+    response.say("Stop screwing around and just call the end user!");
 }
 
 let LongRamble = (slots, session, response, dialogState) => {
